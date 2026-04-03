f31b3ef603afd3c63195bf15b775128df3bc7c57
Tareix <cahidmh30@gmail.com>
2026-04-03 16:23:34 +0000

    done

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
+"""Module for safe division printing."""
 
 
 def safe_print_division(a, b):
+    """Divides two integers and prints the result."""
     try:
-        div = a/b
-    except(ZeroDivisionError):
+        div = a / b
+    except ZeroDivisionError:
         div = None
     finally:
         print("Inside result: {}".format(div))
